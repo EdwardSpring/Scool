7bd8660162b53fcf26370f746df99b0e5acb9f18
edwardspring <eddistics@gmail.com>
2021-03-25 16:26:49 +0100

    init

---
+++ src/main/java/com/scool/AppScoolApplication.java
@@ -0,0 +1,13 @@
+package com.scool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppScoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppScoolApplication.class, args);
+	}
+
+}
